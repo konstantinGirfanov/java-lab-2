ee71b592b2ef594ff989fc603761391307fada59
pe4enka <girfanov_kostia@mail.ru>
2024-02-18 22:58:29 +0500

    bootstrap, mobile adaptation

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,5 +1,7 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     public String getText(){
@@ -9,8 +11,14 @@ public class Post {
     public Integer getLikes(){
         return likes;
     }
-    public Post(String postText, Integer likes){
+    private Date creationDate;
+    public Date getCreationDate()
+    {
+        return creationDate;
+    }
+    public Post(String postText, Date date,Integer likes){
         text = postText;
         this.likes = likes;
+        creationDate = date;
     }
 }
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -5,15 +5,16 @@ import org.springframework.stereotype.Service;
 
 import javax.sound.sampled.Port;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
     public List<Post> listAllPosts(){
         List<Post> result = new ArrayList<Post>();
-        result.add(new Post("Шёл медведь в лес", 0));
-        result.add(new Post("Видит машина горит", 0));
-        result.add(new Post("сел внутрь и сгорел", 0));
+        result.add(new Post("Шёл медведь в лес", new Date(), 0));
+        result.add(new Post("Видит машина горит", new Date(), 0));
+        result.add(new Post("сел внутрь и сгорел", new Date(), 0));
         return result;
     }
 }
